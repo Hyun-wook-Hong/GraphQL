927bd1603c66f5f8fc3413ae1195155c6eeb5d6d
Jeremy Hong <61657215+Hyun-wook-Hong@users.noreply.github.com>
2021-05-11 14:40:43 +0900

    5/11 Mutation implements

--- graphql/db.js
+++ graphql/db.js
@@ -1,36 +1,49 @@
-export const people = [
+export let movies = [
     {
         id: 0,
-        name: "Jeremy",
-        age: 18,
-        gender: "male"
+        name: "The Good, The Bad, The Weird",
+        score: 8.1
     },
     {
         id: 1,
-        name: "Hayley",
-        age: 18,
-        gender: "female"
+        name: "Sympathy for Lady Vengeance",
+        score: 7.5
     },
     {
         id: 2,
-        name: "Nakamura",
-        age: 40,
-        gender: "male"
+        name: "Oldboy",
+        score: 9.04,
     },
     {
         id: 3,
-        name: "Bob",
-        age: 18,
-        gender: "male"
-    },
-    {
-        id: 4,
-        name: "Ashe",
-        age: 18,
-        gender: "female"
+        name: "Minari",
+        score: 8.33,
     },
 ];
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = movies.filter(person => people.id === id);
-    return filteredPeople[0];
-};
+    const filteredMovies = movies.filter(movie => movie.id === id);
+    return filteredMovies[0];
+  };
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length){
+        movie = cleanedMovies;
+        return true;
+    }else{
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        // index starts in 0
+        id: ((movies.length + 1) - 1),
+        name,
+        score,
+    }
+    movies.push(newMovie);
+    return newMovie;
+}
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,12 @@
-import { people, getById } from "./db";
+import { getMovies, getById, addMovie } from "./db";
 // 사용자가 날린 Query를 해결(Resolve)해주는 역할
 const resolvers = {
     Query:{
-        people: () => people,
-        person: () => getById()
+        movies: () => getMovies(),
+        movie: (_, { id }) => getById(id)
+    },
+    Mutation: {
+        addMovie: (_, {name, score}) => addMovie(name, score)
     }
 };
 export default resolvers;
